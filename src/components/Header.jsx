ee6d72c480356b186c859a6a71de9c5e9794edcc
arielle-m <ariem.marii@gmail.com>
2024-03-31 16:49:37 -0700

    fixed header; added site nav & hashlinks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react"
 import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link'
 
 export default function Header() {
@@ -11,16 +13,17 @@ export default function Header() {
     }
 
     return (
-        <header className="site-header">
-            <Link to="/" className="site-logo">
-                {/* <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 512 512">
-                <path d="M256.742 37C407.512 37 501.293 355.711 501.293 355.711C531.717 408.289 493.682 474 432.826 474H79.1676C18.3185 474 -19.717 408.289 10.7069 355.711C10.7069 355.711 91.2167 37 256.742 37Z"></path>
-                </svg> */}
-                <p>Arielle Marin</p>
-            </Link>
+        <header id="masthead" className="site-header">
+            <div className="site-branding">
+                <Link to="/" className="site-logo">
+                    <p className="site-title">Arielle Marin</p>
+                    {/* <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 512 512">
+                    <path d="M256.742 37C407.512 37 501.293 355.711 501.293 355.711C531.717 408.289 493.682 474 432.826 474H79.1676C18.3185 474 -19.717 408.289 10.7069 355.711C10.7069 355.711 91.2167 37 256.742 37Z"></path>
+                    </svg> */}
+                </Link>
+            </div>
   
             <nav className={menuToggle ? "site-navigation toggled" : "site-navigation"} id="site-navigation">
-                {/* hamburger menu button & svg */}
                 <button className="menu-toggle" aria-controls="header-menu" aria-expanded={menuToggle ? "true" : "false"} aria-label="Menu Toggle" onClick={() => menuHandle()}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
                     <title>Menu icon</title>
@@ -30,21 +33,16 @@ export default function Header() {
                 </svg>
                 </button>
 
-                <ul className="nav-menu" id="header-menu">
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <HashLink to='#projects'>Projects</HashLink>
-                    </li>
-                    <li>
-                        <HashLink to='#about'>About</HashLink>
-                    </li>
-                    <li>
-                        <HashLink to='#contact'>Contact</HashLink>
-                    </li>
-                </ul>
+                <nav className="site-navigation">
+                    <ul>
+                    {/* <ul className="nav-menu" id="header-menu"> */}
+                        <li><NavLink to='/' end>Home</NavLink></li>
+                        <li><HashLink to='#projects'>Projects</HashLink></li>
+                        <li><HashLink to='#about'>About</HashLink></li>
+                        <li><HashLink to='#contact'>Contact</HashLink></li>
+                    </ul>
+                </nav>
             </nav>
         </header>
-    );
+    )
 }
