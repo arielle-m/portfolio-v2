f16323f57a8c8f656c980fe5ac2c006f9be22ddc
arielle-m <ariem.marii@gmail.com>
2024-04-01 15:29:39 -0700

    added loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import loading from '../assets/loading.gif';
+
+export default function Loading() {
+    
+    return (
+        <img src={loading} alt="Loading" className="loading" id="loading" />
+    )
+
+}
