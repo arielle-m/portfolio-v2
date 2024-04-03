577ead46182ff3b15e0467636590d1d540bdfcb7
arielle-m <ariem.marii@gmail.com>
2024-04-02 21:42:09 -0700

    finished skills output

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,35 +1,36 @@
 import { useState, useEffect } from 'react'
 import Loading from '../components/Loading'
+import { Link } from 'react-router-dom'
 
 // export default function PageHome( {restBase, fieldImage} ) {
 const PageHome = ( {restBase, fieldImage} ) => {
     const restPath = restBase + 'pages/2'
-    const restPathSkillCategory = restBase + 'skill-category?orderby=name&order=desc'
-    const restPathSkill = restBase + 'skill?orderby=title&order=asc'
+    const restPathSkillDevelopment = restBase + 'skill?_embed&orderby=title&order=asc&per_page=50&skill-category=23'
+    const restPathSkillDesign = restBase + 'skill?_embed&orderby=title&order=asc&per_page=50&skill-category=22'
     const [restData, setData] = useState([])
-    const [restDataSkillCategory, setDataSkillCategory] = useState([])
-    const [restDataSkill, setDataSkill] = useState([])
+    const [restDataSkillDevelopment, setDataSkillDevelopment] = useState([])
+    const [restDataSkillDesign, setDataSkillDesign] = useState([])
     const [isLoaded, setLoadStatus] = useState(false)
 
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch(restPath)
-            const response_skill_category = await fetch(restPathSkillCategory)
-            const response_skill = await fetch(restPathSkill)
-            if ( response.ok && response_skill.ok ) {
+            const response_skill_development = await fetch(restPathSkillDevelopment)
+            const response_skill_design = await fetch(restPathSkillDesign)
+            if ( response.ok && response_skill_development.ok && response_skill_design.ok ) {
                 const data = await response.json()
-                const dataSkillCategory = await response_skill_category.json()
-                const dataSkill = await response_skill.json()
+                const dataSkillDevelopment = await response_skill_development.json()
+                const dataSkillDesign = await response_skill_design.json()
                 setData(data)
-                setDataSkillCategory(dataSkillCategory)
-                setDataSkill(dataSkill)
+                setDataSkillDevelopment(dataSkillDevelopment)
+                setDataSkillDesign(dataSkillDesign)
                 setLoadStatus(true)
             } else {
                 setLoadStatus(false)
             }
         }
         fetchData()
-    }, [restPath, restPathSkill])
+    }, [restPath, restPathSkillDevelopment, restPathSkillDesign])
     
     return (
       <>
@@ -51,23 +52,23 @@ const PageHome = ( {restBase, fieldImage} ) => {
                         <p>{restData.acf.about_paragraph}</p>
                         <p dangerouslySetInnerHTML={{__html: restData.acf.about_hobbies}}></p>
                     <h3>{restData.acf.skills_header}</h3>
-                        {restDataSkillCategory.map (skillcategory =>
-                            <article key={skillcategory.id} id={`tax-${skillcategory.id}`}>
-                                <h4>{skillcategory.name}</h4>
-                                <ul>
-                                {restDataSkill.map ( skill =>
-                                    {skillcategory.id === skill.skill-category[0] && 
-                                        <li key={skill.id} id={`post-${skill.id}`}>{skill.title.rendered}</li>
-                                    }
-                                )}
-                                </ul>
-                            </article>
+                        <h4>{restDataSkillDevelopment[0]._embedded['wp:term'][0][0].name}</h4>
+                        <ul>
+                        {restDataSkillDevelopment.map (skilldevelopment =>
+                            <li key={skilldevelopment.id} id={`post-${skilldevelopment.id}`}>{skilldevelopment.title.rendered}</li>
                         )}
+                        </ul>
+                        <h4>{restDataSkillDesign[0]._embedded['wp:term'][0][0].name}</h4>
+                        <ul>
+                        {restDataSkillDesign.map (skilldesign =>
+                            <li key={skilldesign.id} id={`post-${skilldesign.id}`}>{skilldesign.title.rendered}</li>
+                        )}
+                        </ul>
                 </section>
                 <section id="contact">
                     <h2>{restData.acf.contact_header}</h2>
                     <p>{restData.acf.contact_paragraph}</p>
-                    <Link to={restData.acf.contact_button.url} target={link.project_link.target}>{restData.acf.contact_button.title}</Link>
+                    <Link to={restData.acf.contact_button.url} target={restData.acf.contact_button.target}>{restData.acf.contact_button.title}</Link>
                 </section>
             </article>
         : 
