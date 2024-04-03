1f28cbb94d6736cd813f4e57c7707e5fe20ed3fe
arielle-m <ariem.marii@gmail.com>
2024-04-02 22:00:05 -0700

    added email to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,7 @@ export default function Footer() {
       <nav>
         <ul>
           <li>
+            <Link to={{ pathname: "mailto:ariem.marii@gmail.com" }} target="_blank">Email</Link>
             <Link to={{ pathname: "https://www.linkedin.com/in/ariellemarin/ "}} target="_blank">LinkedIn</Link>
             <Link to={{ pathname: "https://github.com/arielle-m" }} target="_blank">GitHub</Link>
           </li>
