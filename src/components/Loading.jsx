0517fdc59453153b0f3dfa23e8bf59d233040ca1
arielle-m <ariem.marii@gmail.com>
2024-04-07 17:06:21 -0700

    styled loading & added sepia

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import loading from '../assets/loading.gif';
 export default function Loading() {
     
     return (
-        <img src={loading} alt="Loading" className="loading" id="loading" />
+        <img src={loading} alt="Loading" className="loading mx-auto h-32 w-32 sepia brightness-50" id="loading" />
     )
 
 }
