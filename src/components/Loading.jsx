b5d541d347ff65aa87e953b77c08e42c6c71f279
arielle-m <ariem.marii@gmail.com>
2024-04-19 17:15:01 -0700

    centered loading bar

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
 export default function Loading() {
     
     return (
-        <div className="loading-bg flex flex-col min-h-screen w-screen justify-center items-center z-50 fixed top-0">
+        <div className="loading-bg flex flex-col min-h-screen justify-center items-center z-50 fixed top-0 inset-x-0">
             <div className="loading mx-auto h-32 w-32" id="loading">
                 <div></div>
             </div>
