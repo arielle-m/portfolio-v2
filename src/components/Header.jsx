88a3254ee80881b004646bc4aaaede13b31add1e
arielle-m <ariem.marii@gmail.com>
2024-12-02 16:54:48 -0800

    started working on new nav (thumb & sidebar)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 import { NavHashLink } from 'react-router-hash-link'
 
 export default function Header() {
-    const [menuToggle, setMenuToggle]= useState(false)
+    // const [menuToggle, setMenuToggle]= useState(false)
 
-    function menuHandle() {
-        menuToggle ? setMenuToggle(false) : setMenuToggle(true)
-    }
+    // function menuHandle() {
+    //     menuToggle ? setMenuToggle(false) : setMenuToggle(true)
+    // }
 
     return (
         <header id="masthead" className="site-header p-4 z-30">
@@ -17,21 +17,21 @@ export default function Header() {
                 </Link>
             </div>
   
-            <nav className={menuToggle ? "site-navigation toggled" : "site-navigation"} id="site-navigation">
-                <button className="menu-toggle inline-block bg-orange-400 border-4 border-orange-400 rounded-full p-2 focus:outline focus:outline-2 focus:outline-orange-400 fixed right-4 bottom-4 z-40" aria-controls="header-menu" aria-expanded={menuToggle ? "true" : "false"} aria-label="Menu Toggle" onClick={() => menuHandle()}>
+            <nav className="site-navigation" id="site-navigation">
+                {/* <button className="menu-toggle inline-block bg-orange-400 border-4 border-orange-400 rounded-full p-2 focus:outline focus:outline-2 focus:outline-orange-400 fixed right-4 bottom-4 z-40" aria-controls="header-menu" aria-expanded={menuToggle ? "true" : "false"} aria-label="Menu Toggle" onClick={() => menuHandle()}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24">
                     <title>Menu icon</title>
                     <path d="M2 12h20"></path>
                     <path d="M12 2v20"></path>
                 </svg>
-                </button>
+                </button> */}
 
-                <nav className="site-navigation">
-                    <ul id="header-menu" className="bg-orange-700 z-30">
-                        <li><NavHashLink to='/#' smooth end onClick={() => menuHandle()} className="no-underline uppercase font-bold tracking-widest text-right text-orange-100 text-3xl hover:text-orange-300">Home</NavHashLink></li>
-                        <li><NavHashLink smooth to='/#projects' onClick={() => menuHandle()} className="no-underline uppercase font-bold tracking-widest text-right text-orange-100 text-3xl hover:text-orange-300">Projects</NavHashLink></li>
-                        <li><NavHashLink smooth to='/#about' onClick={() => menuHandle()} className="no-underline uppercase font-bold tracking-widest text-right text-orange-100 text-3xl hover:text-orange-300">About</NavHashLink></li>
-                        <li><NavHashLink smooth to='/#contact' onClick={() => menuHandle()} className="no-underline uppercase font-bold tracking-widest text-right text-orange-100 text-3xl hover:text-orange-300">Contact</NavHashLink></li>
+                <nav className="site-navigation bg-orange-700 fixed bottom-0 left-0 w-full h-16 text-center flex justify-center items-center lg:top-2/4 lg:flex-col lg:w-min lg:bg-transparent">
+                    <ul id="header-menu" className="z-30 w-full lg:text-left">
+                        <li className="inline-block w-3/12 lg:w-min"><NavHashLink to='/#' smooth end className="no-underline uppercase font-bold tracking-widest text-right text-orange-100 text-2xl hover:text-orange-300 lg:text-orange-600">Home</NavHashLink></li>
+                        <li className="inline-block w-3/12 lg:w-min"><NavHashLink smooth to='/#projects' className="no-underline uppercase font-bold tracking-widest text-right text-orange-100 text-2xl hover:text-orange-300 lg:text-orange-600">Projects</NavHashLink></li>
+                        <li className="inline-block w-3/12 lg:w-min"><NavHashLink smooth to='/#about' className="no-underline uppercase font-bold tracking-widest text-right text-orange-100 text-2xl hover:text-orange-300 lg:text-orange-600">About</NavHashLink></li>
+                        <li className="inline-block w-3/12lg:w-min lg:w-min"><NavHashLink smooth to='/#contact' className="no-underline uppercase font-bold tracking-widest text-right text-orange-100 text-2xl hover:text-orange-300 lg:text-orange-600">Contact</NavHashLink></li>
                     </ul>
                 </nav>
             </nav>
